fea99f2dc4f05c0827edce3020cf57c51bb85c7c
Danilo Piñeyro <dpineyro@altimetrik.com>
2018-05-07 12:44:06 -0300

    [2] syntax vue

--- public/script.js
+++ public/script.js
@@ -1,6 +1,19 @@
 new Vue({
     el: '#app',
     data: {
-        total: 0
+        total: 0,
+        items: [
+            {title: 'Item 1'},
+            {title: 'Item 2'},
+            {title: 'Item 3'},
+            {title: 'Item 4'}
+            
+        ]
+    },
+    methods: {
+        addItem: function(){
+            console.log('AddITem');
+            this.total += 9.99;
+        }
     }
 });
